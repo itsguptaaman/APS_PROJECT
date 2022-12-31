e184fb7639b08cf7826c38459be9688177cd788d
Aman Gupta <itsamangupta365@gmail.com>
2022-12-31 12:54:21 +0530

    Completed Exception and logger Module

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
-import pymongo
-
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
-
-# Database Name
-dataBase = client["neurolabDB"]
-
-# Collection  Name
-collection = dataBase['Products']
-
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
-
-# Insert above records in the collection
-rec = collection.insert_one(d)
-
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+from sensor.logger import logging
+from sensor.exception import SensorException
+import sys
+import os
 
 
+def test_logger_and_exception():
+    try:
+        logging.info("Starting the test logger function")
+        result = 3/0
+        print(result)
+        logging.info("Stopping the test logger function")
+
+    except Exception as e:
+        logging.debug("Starting the test logger function")
+        raise SensorException(e, sys)
+
+
+if __name__ == "__main__":
+    try:
+        test_logger_and_exception()
+
+    except Exception as e:
+        print(e)
